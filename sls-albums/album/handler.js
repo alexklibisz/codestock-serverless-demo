49ff746ada166bde29f6979fb2993f928d492cac
Alex Klibisz <aklibisz@gmail.com>
2016-06-04 14:27:18 -0400

    endpoint for creating album directory working

--- sls-albums/album/handler.js
+++ sls-albums/album/handler.js
@@ -1,10 +1,22 @@
 'use strict';
+var async = require('asyncawait/async');
+var await = require('asyncawait/await');
 var helpers = require('../lib');
 
-module.exports.handler = function(event, context, cb) {
+module.exports.handler = async(function(event, context, cb) {
 
-  context.succeed({
-    name: helpers.test(event.name)
-  });
+  const bucket = 'demo-codestock-serverless-storage';
 
-};
+  try {
+    event.name = event.name.replace(/\ /g, '_');
+    var directoryMeta = await(helpers.s3CreateDirectory(bucket, event.name, true));
+    context.succeed({
+      status: 'directory created',
+      name: event.name,
+      meta: directoryMeta
+    });
+  } catch(error) {
+    context.fail(error);
+  }
+
+});
--- sls-albums/lib/index.js
+++ sls-albums/lib/index.js
@@ -3,18 +3,19 @@ var aws = require('aws-sdk');
 var mime = require('mime');
 var gm = require('gm').subClass({imageMagick: true});
 
+const s3 = new aws.S3({
+  apiVersion: '2006-03-01',
+  accessKeyId: process.env.AWS_AKI,
+  secretAccessKey: process.env.AWS_SAK,
+  region: 'us-east-1'
+});
+
 /**
  * Take a bucket, image name, and the image body base64-encoded.
  * Store that image in S3.
  * Returns a promise.
  */
 module.exports.s3SaveImage = function s3SaveImage(bucket, name, bodyBase64) {
-  const s3 = new aws.S3({
-    apiVersion: '2006-03-01',
-    accessKeyId: process.env.AWS_AKI,
-    secretAccessKey: process.env.AWS_SAK,
-    region: 'us-east-1'
-  });
   const params = {
     Bucket: bucket,
     ACL: 'public-read',
@@ -31,6 +32,35 @@ module.exports.s3SaveImage = function s3SaveImage(bucket, name, bodyBase64) {
 };
 
 module.exports.s3CreateDirectory = function s3CreateDirectory(bucket, name, overwrite) {
+
+  // Delete the existing directory
+  if(overwrite) {
+
+  }
+
+  // Create the directoy with a single ".keep" file.
+  var meta = {
+    name: name,
+    createdAt: new Date().getTime()
+  };
+  var fname = 'meta.json';
+  fs.writeFileSync(`/tmp/${fname}`, JSON.stringify(meta), 'utf8');
+  var buffer = fs.readFileSync(`/tmp/${fname}`);
+
+  const params = {
+    Bucket: bucket,
+    ACL: 'public-read',
+    Body: buffer,
+    Key: `${name}/${fname}`,
+    ContentType: mime.lookup(fname)
+  };
+
+  return new Promise(function(resolve, reject) {
+    s3.putObject(params, function(error,data) {
+      if(error) reject(error);
+      else resolve(meta);
+    });
+  });
 
 };
 
