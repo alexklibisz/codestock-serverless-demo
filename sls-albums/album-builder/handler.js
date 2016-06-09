ec5998f981342f9a2be12a2bf3e2cf932e95bc96
Alex Klibisz <aklibisz@gmail.com>
2016-06-08 23:43:33 -0400

    sorting images by date in ascending order

--- sls-albums/album-builder/handler.js
+++ sls-albums/album-builder/handler.js
@@ -16,11 +16,13 @@ module.exports.handler = async(function(event, context, cb) {
   const albumName = s3Object.key.split('/').shift();
 
   // Get the files by looking up the albumName directory in S3
+  // Sort the returned Contents array by date in ascending order.
   const objects = await(helpers.s3ListObjects(storageBucket, albumName));
-  const isImage = (x) => mime.lookup(x.Key.split('.').pop()).indexOf('image') > -1;
+  const sorted = objects.Contents.sort((a,b) => new Date(a.LastModified).getTime() > new Date(b.LastModified).getTime());
 
   // Filter out images and then standard and thumbnail sized
-  const images = objects.Contents.filter(isImage);
+  const isImage = (x) => mime.lookup(x.Key.split('.').pop()).indexOf('image') > -1;
+  const images = sorted.filter(isImage);
   const standard = images.filter(x => x.Key.indexOf('thumb') < 0);
   const thumbs = images.filter(x => x.Key.indexOf('thumb') > -1);
 
